f8544e0c1b3b8008b6c3a1c46750773e712d3122
Pierce <piercepokorny.03@gmail.com>
2024-07-19 03:03:13 -0400

    Added animated cards, and other small changes
    -Updated Landing cosmetics
    -Almost finished up first page

--- Serenity/client/src/components/Header.jsx
+++ Serenity/client/src/components/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "/src/styles/Header.css"
 import { Link } from "react-router-dom";
 
 
 
 export default function Header() {
+
+
+
+  
   return (
     <>
     <div className="headerBody">
--- Serenity/client/src/pages/Homepage.jsx
+++ Serenity/client/src/pages/Homepage.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import Landing from '/src/components/Landing.jsx'
-import Header from '/src/components/Header.jsx'
-import "/src/styles/Homepage.css"
-
+import React from 'react';
+import Landing from '/src/components/Landing.jsx';
+import Header from '/src/components/Header.jsx';
+import "/src/styles/Homepage.css";
 
 export default function Homepage() {
   return (
-
     <>
-    
-    <Landing/>
-    <Header/>
-    <div className="homepageBody">
-      homepage
-    </div>
-    
+      <Landing />
+      <Header />
+      <div className="homepageBody">
+        <div className="hpFirstPage">
+          <div className="hp1Title">
+            <h2>Our Treatments</h2>
+          </div>
+          <div className="hp1CardContainer">
+            <div className="hp1card">
+              <div className="cardFace cardFront">
+                <div className="top"><img src="/src/assets/images/img4.jpg" alt="" />
+                </div>
+                <div className="bottom">
+                <h1>Hair Removal</h1>
+                </div>
+              </div>
+              <div className="cardFace cardBack">
+                <h1>Back Side</h1>
+              </div>
+             
+            </div>
+            <div className="hp1card">
+              <div className="cardFace cardFront">
+                <div className="top">
+                <img src="/src/assets/images/img5.jpg" alt="" />
+                </div>
+                <div className="bottom">
+                  <h1>Skin Rejuvenation</h1>
+                </div>
+              </div>
+              <div className="cardFace cardBack">
+                <h1>Back Side</h1>
+              </div>
+              
+            </div>
+
+            <div className="hp1Info">
+              Our tools
+           
+              
+            </div>
+          </div>
+
+
+        </div>
+        <div className="hpSecondPage"></div>
+      </div>
     </>
-   
-  )
+  );
 }
