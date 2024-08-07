5e34b2d6fdca302b8f8ea236779351efa7943cda
Pierce <piercepokorny.03@gmail.com>
2024-08-07 01:44:01 -0400

    Started work on about page

--- Serenity/client/src/pages/AboutPage.jsx
+++ Serenity/client/src/pages/AboutPage.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Header from '/src/components/Header.jsx';
-
+import Footer from '/src/components/Footer.jsx'
+import '/src/styles/Aboutpage.css'
 
 export default function AboutPage() {
   return (
     <>
     <Header/>
-    <div>About</div>
+    
+    <div className="aboutPageBody">
+      <div className="ap1">
 
+      <div className="ap1Sec1">
+        <div className="ap1Sec1L">
+          <h2>How does it work?</h2>
+        </div>
+        <div className="ap1Sec1R"></div>
+      </div>
+      <div className="ap1Sec1">
+      <div className="ap1Sec1L"></div>
+      <div className="ap1Sec1R"></div>
+      </div>
+
+      </div>
+    </div>
+
+
+    <Footer/>
     </>
   )
 }
